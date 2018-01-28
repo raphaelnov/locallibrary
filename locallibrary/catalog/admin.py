9e3ea9543a956c8b8e04ca68c66fb0d35e0764c0
raphnov <raphael.novena@yahoo.com.br>
2018-01-28 09:09:56 -0200

    admin and first book

--- locallibrary/catalog/admin.py
+++ locallibrary/catalog/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Author, Book, Genre, BookInstance, Language
+
+admin.site.register(Author)
+admin.site.register(Book)
+admin.site.register(Genre)
+admin.site.register(BookInstance)
+admin.site.register(Language)
