dd4d597e8f76b90b422f487799d687e38ce84f6a
raphnov <raphael.novena@yahoo.com.br>
2018-02-02 22:26:58 -0200

    users go on fire

--- locallibrary/catalog/models.py
+++ locallibrary/catalog/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.urls import reverse #Used to generate the URL by reversing the URL patterns
 import uuid
+from django.contrib.auth.models import User
+from datetime import date
 # Create your models here.
 
 class Genre(models.Model):
@@ -69,6 +71,7 @@ class BookInstance(models.Model):
     book = models.ForeignKey('Book', on_delete=models.SET_NULL, null=True)
     imprint = models.CharField(max_length=200)
     due_back=models.DateField(null=True, blank=True)
+    borrower = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
 
     LOAN_STATUS=(
         ('m', 'Maintanance'),
@@ -81,6 +84,12 @@ class BookInstance(models.Model):
 
     class Meta:
         ordering = ['due_back']
+
+    @property
+    def is_overdue(self):
+        if self.due_back and date.today() > self.due_back:
+            return True
+        return False
 
     def __str__(self):
         """
